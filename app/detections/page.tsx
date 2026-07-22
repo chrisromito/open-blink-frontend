103cf8c176f35908d2273807cddc568d6b54bb3b
Chris Romito <chris@modii.co>
2026-07-22 13:56:29 -0600

    main - Cleanup & fix docker workflow

--- app/detections/page.tsx
+++ app/detections/page.tsx
@@ -1,17 +1,22 @@
 'use client'
-import {Group, Stack} from '@mantine/core'
-import {useState} from 'react'
-import LabelGroup from '@/app/detections/components/LabelGroup'
-import DeviceSelect from '@/app/detections/components/DeviceSelect'
+import {Group, MultiSelect, Stack} from '@mantine/core'
+import {useEffect, useState} from 'react'
 import DetectionCard from '@/app/detections/components/DetectionCard'
 import {getUrl} from '@/app/config'
-import {TDetection} from '@/app/types'
+import {TDetection, TDevice} from '@/app/types'
 import {sortByCreatedAt} from '@/app/lib/sortBy'
+import {getDevices, getLabels} from '@/app/lib/devices'
 
 
 export default function DetectionsPage() {
-    const [deviceIds, setDeviceIds] = useState<number[]>([])
+    // Labels
     const [labels, setLabels] = useState<string[]>([])
+    const [selectedLabels, setSelectedLabels] = useState<string[]>([])
+    // Devices
+    const [devices, setDevices] = useState<TDevice[]>([])
+    const [selectedIds, setSelectedIds] = useState<number[]>([])
+
+    // Detections
     const [detections, setDetections] = useState<TDetection[]>([])
 
     const onFiltersChange = async () => {
@@ -19,7 +24,7 @@ export default function DetectionsPage() {
         labels.forEach(label => {
             params.append('label', label)
         })
-        deviceIds.forEach(did => {
+        selectedIds.forEach(did => {
             params.append('device_id', String(did))
         })
         const res: TDetection[] = await fetch(getUrl('api/detection-image', params))
@@ -27,24 +32,67 @@ export default function DetectionsPage() {
         setDetections(sortByCreatedAt(res))
     }
 
+    useEffect(() => {
+        const controller = new AbortController()
+        getLabels(controller)
+            .then(ls => {
+                const sortedLabels: string[] = ls.toSorted()
+                setLabels(sortedLabels)
+                setSelectedLabels(sortedLabels)
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    return Promise.resolve([])
+                }
+                throw err
+            })
+
+        getDevices(controller)
+            .then(ds => {
+                setDevices(ds)
+                setSelectedIds(ds.map(({id}) => id))
+                return ds
+            })
+            .then(onFiltersChange)
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    return Promise.resolve([])
+                }
+                return Promise.reject(err)
+            })
+
+        return () => {
+            controller.abort()
+        }
+    }, [])
+
     return (
         <div className="w-full">
             {/* Filter Form */}
             <Group className={"p-4"}>
                 <div className={"px-4"}>
-                    <LabelGroup
-                        onChange={ls => {
-                            setLabels(ls)
-                            return onFiltersChange()
-                        }}
+                    <MultiSelect
+                        label="Labels"
+                        data={labels}
+                        onChange={value => setSelectedLabels(value)}
+                        value={selectedLabels}
                     />
                 </div>
                 <div className={"px-4"}>
-                    <DeviceSelect
-                        onChange={ds => {
-                            setDeviceIds(ds)
-                            return onFiltersChange()
+                    <MultiSelect
+                        label="Devices"
+                        data={devices.map(({id, name}) => (
+                            {
+                                value: String(id),
+                                label: name
+                            }
+                        ))}
+                        onChange={value => {
+                            const ids: number[] = value.map(Number)
+
+                            setSelectedIds(ids)
                         }}
+                        value={selectedIds.map(String)}
                     />
                 </div>
             </Group>
