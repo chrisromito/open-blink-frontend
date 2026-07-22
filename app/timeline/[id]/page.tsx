d8863c303f55b074b91216349e33805d2963eb48
Chris Romito <chris@modii.co>
2026-07-22 15:17:28 -0600

    main - Add timeline event detail page to make mobile navigation more intuitive

--- app/timeline/[id]/page.tsx
+++ app/timeline/[id]/page.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import { MdOutlineNavigateBefore } from "react-icons/md";
+import { Group, Stack } from '@mantine/core'
 import EventCard from '@/app/timeline/components/EventCard'
 
 
@@ -7,6 +10,14 @@ export default async function EventDetailPage({params}: { params: TParam }) {
     const {id} = await params
 
     return (
-        <EventCard eventId={id}/>
+        <Stack>
+            <Link href={`/timeline`}>
+                <Group>
+                    <MdOutlineNavigateBefore />
+                    Back
+                </Group>
+            </Link>
+            <EventCard eventId={id}/>
+        </Stack>
     )
 }
