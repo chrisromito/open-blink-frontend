ffde89cb3922f5f2eae35c876e682dd90c5d03e2
Chris Romito <chris@modii.co>
2026-07-22 15:06:22 -0600

    main - Add timeline event detail page to make mobile navigation more intuitive

--- app/timeline/[id]/page.tsx
+++ app/timeline/[id]/page.tsx
@@ -3,8 +3,8 @@ import EventCard from '@/app/timeline/components/EventCard'
 
 type TParam = Promise<{ id: number }>
 
-export default async function EventDetailPage({ params }: TParam) {
-    const { id } = await params
+export default async function EventDetailPage({params}: { params: TParam }) {
+    const {id} = await params
 
     return (
         <EventCard eventId={id}/>
